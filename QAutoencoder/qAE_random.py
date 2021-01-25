77d3bc637a9613fc38dd03f2a631c25c7b295613
Burak Mete <burak.mete11@gmail.com>
2021-01-25 01:10:49 +0100

    tensorflow model is ready

--- QAutoencoder/qAE_random.py
+++ QAutoencoder/qAE_random.py
@@ -43,6 +43,15 @@ from qCircuit import Net, latNet, genNet
 from visualizations import visualize, visualize_state_vec
 from copy import deepcopy
 import seaborn
+import tensorflow as tf
+
+#%% numpy implementations for the gates
+
+
+I  = np.array(((0j + 1, 0), (0, 1)))
+pauli_x  = np.array(((0j, 1), (1, 0)))
+pauli_y = np.array(((0, -1j), (1j, 0)))
+pauli_z = np.array(((1+0j, 0), (0, -1)))
 
 # %%
 def Fidelity_loss(mes):
@@ -57,7 +66,7 @@ dev2 = qml.device("default.qubit", wires=4,shots = 1000)
 model = Net(dev2, 1, 4, 2, 1)
 
 # new_model = Net(dev2, 1, 4, 2, 1)
-model.load_state_dict(torch.load('checkpoint.pth.tar'))
+model.load_state_dict(torch.load('21jan.pth.tar'))
 
 learning_rate = 0.01
 epochs = 5
@@ -207,7 +216,6 @@ def qubitsToStateVector(vec, n):
 
 # %%
         
-,
 total_losses = np.zeros((50) , float)
 # %%
 batches = np.arange(n_train_samples)
@@ -268,18 +276,23 @@ with torch.no_grad():
         # total_losses[i] = loss_func(output2)
 
 
-# %% 
+# %% Extracting the parameters, to select suitable training data
 first_rots , final_rots ,cnots ,wires_list = model.paramServer()
 # %%
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.visualization import plot_histogram, plot_bloch_vector
 from math import sqrt, pi
 import qiskit
-# %% 
+# %%  This part is implemented to extract some qubits, which can be 
+# succesfully d/encoded. Thus, we make sure that if the second network in the
+# pipeline works, whole system should work
+
+dev_qml = qml.device("default.qubit", wires=3,shots = 1000)
 lat_dev_qml = qml.device("default.qubit", wires=3,shots = 1000)
 gen_qml = qml.device("default.qubit", wires=3,shots = 1000)
 lat_loss = np.zeros((200) , float)
 output_loss = np.zeros((200,4) , float)
+
 @qml.qnode(dev_qml)
 def penny(inputs,fir,cnot,last):
     qml.templates.AmplitudeEmbedding(inputs, wires = [1,2], normalize = True,pad=(0.j))
@@ -368,19 +381,183 @@ for q in (selected_qubits):
     selected_latent_features.append(penny(train_qubits[q].detach().numpy()   ,first_rots, cnots, final_rots)[1])
     selected_features.append(train_qubits[q].detach().numpy() )
     
-features= tf.convert_to_tensor(selected_features)
-latents = tf.convert_to_tensor(selected_latent_features)
+features = tf.convert_to_tensor(selected_features[:7] , dtype_hint=tf.complex128)
+latents = tf.convert_to_tensor(selected_latent_features[:7]  , dtype_hint=tf.complex128)
 
-# np.sqrt(gen_penny(penny(train_qubits[6].detach().numpy() ,first_rots , cnots, final_rots)[1]))
+targets = tf.convert_to_tensor(selected_features[7:] , dtype_hint=tf.complex128)
+target_latents = tf.convert_to_tensor(selected_latent_features[7:] , dtype_hint=tf.complex128)
+ 
+# %% 
 
-generate_penny.draw()
-# np.argsort(lat_loss)
 
-# selected_feature_qubits_actual = []
-# selected_feature_qubits_latent = []
-# selected_qubits = np.argsort(lat_loss)[:20]
-# for i in selected_qubits:
+
+# dev = qml.device('default.qubit', wires=2 , shots = 10000)
+class ExpMat(tf.Module):
+    def __init__(self, name=None):
+        super().__init__(name=name)
+        # self.h  = tf.Variable(
+        #         initial_value=H,
+        #         trainable=False,
+        #         dtype = tf.complex128,
+        #         name = 'h'
+        #     )
+        self.pauli_x  = tf.Variable(
+                initial_value=pauli_x,
+                trainable=False,
+                dtype = tf.complex128
+            )   
+        self.pauli_y  = tf.Variable(
+                initial_value=pauli_y,
+                trainable=False,
+                dtype = tf.complex128
+            )   
+        self.pauli_z  = tf.Variable(
+                initial_value=pauli_z,
+                trainable=False,
+                dtype = tf.complex128
+            )  
+        self.I  = tf.Variable(
+                initial_value=I,
+                trainable=False,
+                dtype = tf.complex128
+            )
+        self.t  = tf.Variable(
+                initial_value=1,
+                trainable=False,
+                dtype = tf.complex128,
+                name = 't'
+            )
+        
+        self.b  = tf.Variable(
+                initial_value=2,
+                trainable=True,
+                dtype = tf.float64,
+                name = 'b'
+            )
+        self.c  = tf.Variable(
+                initial_value=3,
+                trainable=True,
+                dtype = tf.float64,
+                name = 'c'
+            )
+        self.d  = tf.Variable(
+                initial_value=1,
+                trainable=True,
+                dtype =tf.float64,
+                name = 'd'
+            )
+        
+        self.bc  = tf.Variable(
+                initial_value=0,
+                trainable=True,
+                dtype =tf.complex128,
+                name = 'bc'
+            )
+        self.cc  = tf.Variable(
+                initial_value=0,
+                trainable=True,
+                dtype =tf.complex128,
+                name = 'cc'
+            )
+        self.dc  = tf.Variable(
+                initial_value=0,
+                trainable=True, # TODO: make it non trainable
+                dtype =tf.complex128,
+                name = 'dc'
+            )
+        self.a2 = tf.complex(self.b,self.b)
+        self.a3 = tf.complex(self.c,self.c)
+        self.a4 = tf.complex(self.d,self.d)
+        phasor_qml = qml.device("default.qubit", wires=1,shots = 1000)
+        @qml.qnode(phasor_qml)
+        def get_probs(inputs):
+            qml.templates.AmplitudeEmbedding(inputs, wires = [0], normalize = True,pad=(0.j))    
+            return qml.probs([0])
+        weight_shapes = {}
+        self.qlayer = qml.qnn.KerasLayer(get_probs, weight_shapes, output_dim=2)
+    def __call__(self, x, t):
+        """returns a matrix multiplication
+        
+        
+        Parameters
+        ----------
+        x : tensor
+        input
+        
+        Returns
+        -------
+        tensor
+        output
+        
+        """
+        
+        # return tf.linalg.expm(-1j * (self.pauli_x * self.b + self.pauli_y * self.c + self.pauli_z * self.d ) )
+        l = tf.linalg.expm(-1j * (self.pauli_x * self.bc + self.pauli_y * self.cc + self.pauli_z * self.dc ) * t)
+        
+        l = tf.linalg.matvec( l , x)
+        return tf.math.abs(l[0] ** 2) , tf.math.abs(l[1] ** 2)
+        # return self.qlayer((l[0] , l[1]))
+
+   
+
+# %%  Network to get rid of the phasor and getting the probabilities,
+# TODO: Relocate it inside the ExpMat class
+phasor_qml = qml.device("default.qubit", wires=1,shots = 1000)
+@qml.qnode(phasor_qml)
+def get_probs(inputs):
+    qml.templates.AmplitudeEmbedding(inputs, wires = [0], normalize = True,pad=(0.j))    
+    return qml.probs([0])
+
+
+val = [-0.42244875-0.5698601j ,  0.10250422+0.15938799j]
+np.abs(val[0])  + (np.abs(val[1]))
+tf.convert_to_tensor(val)
+U = np.array([[-0.16055654-0.5698601j, -0.5698601 -0.5698601j],
+       [ 0.5698601 -0.5698601j, -0.16055654+0.5698601j]])
+
+
+U = np.matrix(U)
+U.H @ U
+[[-0.16055654-0.5698601j -0.5698601 -0.5698601j]
+ [ 0.5698601 -0.5698601j -0.16055654+0.5698601j]]
+
+
+tf.Tensor([-0.55218828-0.79782213j  0.21355806+0.11384599j], shape=(2,), dtype=complex128)
+amps_deneme = U @ np.sqrt(psi_in)
+(amps_deneme[0,0], amps_deneme[0,1])
+
+
+matrix([[  0.21355806+0.113846j  ]])
+
+np.abs((-0.55218828-0.79782213j) ** 2)
+np.abs((0.21355806+0.113846j) ** 2)
+# %%  
+expMat = ExpMat(name="expmat")
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+loss_history = []
+
+t_count = 1
+def train_step(psi_in, psi_out):
+    with tf.GradientTape() as tape:
+        # res = expMat(psi_in)
+        # for t in range(t_count): to be implemented later on
+        
+        res = expMat(tf.math.sqrt(psi_in) , 1)
+        
+        # loss_value = tf.reduce_sum(tf.pow( tf.math.real(res) - tf.math.real(psi_out),2))/(2) + tf.reduce_sum(tf.pow( tf.math.imag(res) - tf.math.imag(psi_out),2))/(2)
+        loss_value = tf.reduce_sum(tf.pow(psi_out  - res,2))/(2)
+        
+    loss_history.append(loss_value.numpy().mean())
     
+    grads = tape.gradient(loss_value, expMat.trainable_variables)
+    optimizer.apply_gradients(zip(grads,expMat.trainable_variables))
+
+for j in range(200):
+    for i in range(len(features)):
+        train_step(latents[i], target_latents[i])
+    
+psi_in  = latents[i]
+psi_out = target_latents[i]
 # %%
 '''
 lat_loss = np.zeros((200) , float)
